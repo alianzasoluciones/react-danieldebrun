a50d68a0d3780bb4fc4d8419aae4e5a2733a9e9d
daniel <danieldebrun87@hotmail.com>
2022-05-18 20:15:59 -0300

    entregaClase3

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,24 +1,36 @@
 import  { useState } from 'react'
 
 const ItemCount = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
      
   const decrement = () => {
-          setCount((count) => count -1 )
+    if (count <= 0 ){
+      setCount (1)
+    } else {
+      setCount (count - 1)
+    }
+    
+          setCount((count) => count - 0 )
       }
 
  
 const increment = () => {
+  if (count === 4 ){
+    setCount (1)
+  } else {
+    setCount (count + 0)
     
      setCount (count + 1)
  }
- 
+}
 
  return(
     <div class="container-fluid">
     <div class="d-grid gap-2 d-md-block">
     <button onClick={decrement} class="btn btn-primary" type="button" >-</button>
     <h2 style={{position:"flex", display:"inline",padding:10}}>{count}</h2>
+    <h3>{ count <= 3 ? "tenemos stock" : "no tenemos stock" 
+      } </h3>
     <button onClick={increment} class="btn btn-primary" type="button">+</button>
   </div>
   </div>
--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,5 +1,8 @@
+
+
 const ItemListContainer = (props) => {
     return <h1>{props.greeting}</h1>
+    
 }
 
 export default ItemListContainer
