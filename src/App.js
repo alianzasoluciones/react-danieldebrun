8c606e517c24220e5e21cabe40bbe629477206bd
daniel <danieldebrun87@hotmail.com>
2022-06-05 20:09:25 -0300

    duda de boton

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <Route path='/' element={<ItemListContainer greeting="Todos los Productos"/>}/>
             <Route path='/category/:categoryId' element={<ItemListContainer greeting="Productos que te gustaran"/>}/>
             <Route path='/descripcion/:productId' element={<ItemDetailContainer />}/>
+            
             <Route path='/about' element={<h1>Atras</h1>}/>
             <Route path='*' element={<h1>PAGE NOT FOUND 404</h1>} />
 
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -12,7 +12,7 @@ const InputCount = ({onConfirm, stock, inicial= 1}) => {
     return (
         <div>
             <input type='number' onChange={handleChange} value={count}/>
-            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+            <button onClick={ () => onConfirm(count)}>Agregar al carrito</button>
         </div>
     )
 }
@@ -25,21 +25,23 @@ const ButtonCount = ({onConfirm, stock, inicial= 1}) => {
         }
     }
 
-    const restar = ()=> {
-        setCount(count - 1)
-    }
+    const restar = () => {
+        if (count > 1 ){
+          setCount (count - 1)
+        }
+          };
     return (
         <div>
             <p>{count}</p>
             <button onClick={restar}>-</button>
             <button onClick={sumar} >+</button>
-            <button style={{fontSize:12, }} onclick={()=> onConfirm(count)}>Agregar al carrito</button>
+            <button  onclick={ () => onConfirm(count)}>Agregar al carrito</button>
         </div>
     )
 }
 
 const ItemDetail = ({stock, imagen,descripcion,precio , name, category}) => {
-        const [inputType]= useState('input')
+        const [inputType, setInputType]= useState('input')
         const [quantity, setQuantity] = useState(0)
         
         const ItemCount = inputType === 'input' ? InputCount : ButtonCount
@@ -50,6 +52,7 @@ const ItemDetail = ({stock, imagen,descripcion,precio , name, category}) => {
 
     return(
         <div class="card mb-3" style={{maxWidth: 540,background:'silver',textAlign:'center'}}>
+            <button onClick={() => setInputType(inputType === 'input' ? 'button' : 'input')}>cambiar Contador</button>
   <div class="row g-0">
     <div class="col-md-4">
       <img img src={imagen} class="img-fluid rounded-start" alt="{name}"/>
@@ -65,6 +68,7 @@ const ItemDetail = ({stock, imagen,descripcion,precio , name, category}) => {
   </div>
   <footer style={{textAlign:'center'}}>
       { quantity > 0 ? <Link to='/cart'>Finalizar compra</Link>: <ItemCount stock={stock} onConfirm={handleOnAdd}/>} 
+     <div> <Link to='/'>seguir comprando</Link> </div>
   </footer>
 </div>
 
